450ceda7b26194f77919bf331f0b194db1c6a3a6
Christoph Werner <codepunkt@users.noreply.github.com>
2021-06-08 08:47:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const got = require('got');
+const fs = require('fs/promises');
+
+(async () => {
+  try {
+    const response = await got('https://impfterminservice-kvwl.service-now.com/');
+    const currentBody = response.body
+    const previousBody = await fs.readFile('current.html', 'utf-8');
+
+    if (currentBody === previousBody) {
+      process.exit(1);
+    } else {
+      await fs.writeFile('current.html', currentBody);
+    }
+  } catch (error) {
+    console.log(error.response.body);
+  }
+})();
