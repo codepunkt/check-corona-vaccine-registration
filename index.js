106c9826c16913b7fab218429b83fea2f3fffdbe
Christoph Werner <codepunkt@users.noreply.github.com>
2021-06-08 11:14:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const got = require('got');
 const fs = require('fs/promises');
 
 const cacheFile = 'content.html';
+// const siteToCheck = 'https://impfterminservice-kvwl.service-now.com/'
+const siteToCheck = 'https://codepunkt.de/'
 
 (async () => {
   try {
-    const response = await got('https://impfterminservice-kvwl.service-now.com/');
+    const response = await got(siteToCheck);
     const currentBody = response.body
     const previousBody = await fs.readFile(cacheFile, 'utf-8');
 
